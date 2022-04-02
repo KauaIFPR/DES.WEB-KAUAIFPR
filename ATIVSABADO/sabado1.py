020a1c14f6ea9982a1f768c1d4f8ff1ef279ac5a
Kauã Matheus Callegari Ribeiro <100441524+KauaIFPR@users.noreply.github.com>
2022-04-01 21:31:53 -0300

    Add files via upload

---
+++ ATIVSABADO/sabado1.py
@@ -0,0 +1,15 @@
+class Pessoas:
+    def __init__(self, nome, peso, nascionaliade, genero, idade):
+        print("Construindo uma nova pessoa... :) {}".format(self))
+        self.idade = idade
+        self.genero = genero
+        self.nascionaliade = nascionaliade
+        self.peso = peso
+        self.nome = nome
+
+       def andar(self):
+
+    def comer(self):
+
+ def conversar(self):
+
